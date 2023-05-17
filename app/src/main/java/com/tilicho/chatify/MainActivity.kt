68fb8625f743fb48b0bbff7181413ece177bc617
Demudu Swamy <swamyrallapalli@gmail.com>
2023-05-17 15:34:11 +0530

    Changed package name

--- app/src/main/java/com/tilicho/chatify/MainActivity.kt
+++ app/src/main/java/com/tilicho/chatify/MainActivity.kt
@@ -6,19 +6,9 @@ import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.intPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
-import androidx.datastore.preferences.rxjava2.RxPreferenceDataStoreBuilder
-import androidx.datastore.rxjava2.RxDataStore
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.tilicho.chatify.navigation.Screen
@@ -26,12 +16,6 @@ import com.tilicho.chatify.navigation.SetUpNavGraph
 import com.tilicho.chatify.ui.theme.SimpleChatTheme
 import com.tilicho.chatify.viewmodel.AuthViewModel
 import com.tilicho.chatify.viewmodel.ChatViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import okhttp3.Dispatcher
 
 class MainActivity : ComponentActivity() {
     private lateinit var navController: NavHostController
@@ -54,7 +38,8 @@ class MainActivity : ComponentActivity() {
                 val lifecycleOwner = LocalLifecycleOwner.current
                 /*val isUserRegistered = UserDataStore(this).getUid().collectAsState(initial = "").value*/
 
-                chatViewModel = ChatViewModel(application = application, lifecycleOwner = lifecycleOwner)
+                chatViewModel =
+                    ChatViewModel(application = application, lifecycleOwner = lifecycleOwner)
                 navController = rememberNavController()
                 SetUpNavGraph(
                     context = this@MainActivity,
@@ -64,7 +49,9 @@ class MainActivity : ComponentActivity() {
                     lifecycleOwner = lifecycleOwner
                 )
 
-                if (UserDataStore(this).getUid().collectAsState(initial = "").value?.isNotEmpty() == true) {
+                if (UserDataStore(this).getUid()
+                        .collectAsState(initial = "").value?.isNotEmpty() == true
+                ) {
                     navController.navigate(Screen.ChatsScreen.route) {
                         popUpTo(Screen.RegisterScreen.route) {
                             inclusive = true
